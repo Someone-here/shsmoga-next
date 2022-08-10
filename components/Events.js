7fb50f88dd35fe9d46d11d58bd0f8eac8d874d05
Esh <eshgupta1217@gmail.com>
2022-08-10 18:35:51 +0530

    img

--- components/Events.js
+++ components/Events.js
@@ -2,7 +2,6 @@ import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/react-splide/css";
 import React from "react";
 import styles from "../styles/Events.module.css";
-import Image from "next/image";
 
 export default function Events() {
   return (
@@ -17,28 +16,28 @@ export default function Events() {
       }}
     >
       <SplideSlide className={styles.slide}>
-        <Image
+        <img
           src="https://shsmoga.com/assets/images/recentnews/QS.png"
           alt="Image 1"
           className={styles.image}
         />
       </SplideSlide>
       <SplideSlide className={styles.slide}>
-        <Image
+        <img
           src="https://shsmoga.com/assets/images/recentnews/download.png"
           alt="Image 2"
           className={styles.image}
         />
       </SplideSlide>
       <SplideSlide className={styles.slide}>
-        <Image
+        <img
           src="https://shsmoga.com/assets/images/recentnews/affiliations4.jpg"
           alt="Image 3"
           className={styles.image}
         />
       </SplideSlide>
       <SplideSlide className={styles.slide}>
-        <Image
+        <img
           src="https://shsmoga.com/assets/images/recentnews/ncc.png"
           alt="Image 4"
           className={styles.image}
